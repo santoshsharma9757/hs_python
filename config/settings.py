3d9b3a076313dbb54cb6cf494ed90c4f763b6195
santosh_ast <santosh.lohar@appliedenergytechnologies.com>
2026-01-21 11:21:02 +0530

    updated cors error

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "account",
     "todo",
     # SimpleJWT for JWT authentication
@@ -63,11 +64,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# To allow everyone (for testing)
+CORS_ALLOW_ALL_ORIGINS = True 
+
+# OR Allow specific domains (recommended for production)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    # Add your production domain here
 ]
 
 ROOT_URLCONF = "config.urls"
